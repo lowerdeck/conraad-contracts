fb4402ffdcb1a45a79b307c1b6cd0e5cfbe5fa06
Joost Lubach <joostlubach@gmail.com>
2026-07-25 15:59:18 +0200

    Lint config

--- src/sections/conditional.ts
+++ src/sections/conditional.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod'
 
 export function $if<T extends z.ZodType<any>>(base: T) {
-  return z.object({
-    $if:  z.string().max(255),
-    then: base,
-    else: base.optional(),
-  })
+  return z.union([
+    z.object({
+      $if:  z.string().max(255),
+      then: base,
+      else: base.optional(),
+    }),
+    base,
+  ])
 }
 
 export type If<T> = z.output<ReturnType<typeof $if<z.ZodType<T>>>>
