e99ccc9620bc80a4be3abe56aead89d48b334eb7
Joost Lubach <joostlubach@gmail.com>
2026-07-30 12:54:17 +0200

    Going bacjk

--- src/structure/common.ts
+++ src/structure/common.ts
@@ -3,6 +3,6 @@ import { z } from 'zod'
 export const contractSectionCommon = {
   name:         z.string().max(255),
   title:        z.string().max(255),
-  counter:      z.boolean().default(false),
+  counter:      z.string().optional(),
   render_title: z.boolean().default(true),
 }
