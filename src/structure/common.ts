5686bb0e5d045a4f45c40b30a45128a32345bd7a
Joost Lubach <joostlubach@gmail.com>
2026-08-08 11:49:48 +0200

    Adding inputs

--- src/structure/common.ts
+++ src/structure/common.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod'
+import { input } from './input'
 
 export const contractSectionCommon = {
   name:         z.string().max(255),
   title:        z.string().max(255),
   counter:      z.string().optional(),
   render_title: z.boolean().default(true),
+  inputs:       z.record(z.string(), input).default({}),
 }
---
+++ src/structure/input.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod'
+
+export const validation = z.object({
+  rule:    z.string().max(255),
+  message: z.string().max(255),
+})
+
+const inputCommon = {
+  label:    z.string().max(255),
+  validate: z.array(validation).default([]),
+}
+
+export const textInput = z.object({
+  type:       z.literal('text'),
+  min_length: z.number().optional(),
+  max_length: z.number().optional(),
+  pattern:    z.string().optional(),
+  default:    z.string(),
+  ...inputCommon,
+})
+
+export const numberInput = z.object({
+  type:    z.literal('number'),
+  min:     z.number().optional(),
+  max:     z.number().optional(),
+  step:    z.number().optional(),
+  default: z.number(),
+  ...inputCommon,
+})
+
+export const input = z.discriminatedUnion('type', [
+  textInput,
+  numberInput,
+])
