b1674b52abe7533d0b9b19d95934a9dd04936a1a
Joost Lubach <joostlubach@gmail.com>
2026-09-24 12:19:43 +0200

    Sorry not

--- src/structure/list.ts
+++ src/structure/list.ts
@@ -9,7 +9,7 @@ export function listItem(level: number): z.ZodType<ListItem> {
     $if:  expression().optional(),
     text: z.string(),
     ...(level < 6 && {
-      items: z.array(listItem(level + 1)).default([]),
+      items: z.array(listItem(level + 1)).optional(),
     }),
   }) as z.ZodType<ListItem>
 }
